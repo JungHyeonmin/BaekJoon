c61fb71cafb716dbc850c0f859ed9bb8b4f633e7
MungChi <2ee42a45@gmail.com>
2024-08-30 00:34:11 +0900

    [Bronze V] Title: A+B - 3, Time: 184 ms, Memory: 17888 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt(); // 반복 횟수
+
+        // 반복해서 입력
+        for (int i = 1; i <= T; i++) {
+
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A + B);
+        }
+    }
+}
